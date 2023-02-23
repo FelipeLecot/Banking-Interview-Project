f3f472219d5977d05c319a2625ff81cd7167ca67
Agustin Moscato <agustinmoscato369@gmail.com>
2023-02-23 11:42:07 -0300

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
-import React, {Fragment, useState, useEffect} from "react";
-
-import {Navbar} from "./components/Navbar";
-import {CreateAccount} from "./components/CreateAccount";
-import {DeleteAccount} from "./components/DeleteAccount";
-import {GetBalances} from "./components/GetBalances";
-import {CreditBalance} from "./components/CreditBalances";
-import {TransferBalances} from "./components/TransferBalances";
-import {DebitBalance} from "./components/DebitBalances";
+import React, { Fragment, useState, useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { CreateAccount } from "./components/CreateAccount";
+import { DeleteAccount } from "./components/DeleteAccount";
+import { GetBalances } from "./components/GetBalances";
+import { CreditBalance } from "./components/CreditBalances";
+import { TransferBalances } from "./components/TransferBalances";
+import { DebitBalance } from "./components/DebitBalances";
 
 import './index.css';
 
-export function App() {
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div className="container mainScreen"><CreateAccount /></div>,
+    },
+    {
+        path: "/DeleteAccount",
+        element: <div className="container mainScreen"><DeleteAccount /></div>,
+    },
+    {
+        path: "/GetBalances",
+        element: <div className="container mainScreen"><GetBalances /></div>,
+    },
+    {
+        path: "/CreditBalance",
+        element: <div className="container mainScreen"><CreditBalance /></div>,
+    },
+    {
+        path: "/DebitBalance",
+        element: <div className="container mainScreen"><DebitBalance /></div>,
+    },
+    {
+        path: "/TransferBalances",
+        element: <div className="container mainScreen"><TransferBalances /></div>,
+    },
+]);
 
-     
-    return(
+
+export function App() {
+    return (
         <Fragment>
-            <Navbar/>
-            <div className="container mainScreen">
-                <CreateAccount />
-                <DeleteAccount/>
-            </div>
-            <div className="container mainScreen">
-              <GetBalances/>
-              <CreditBalance/>
-              <DebitBalance/>
-              <TransferBalances/>
-            </div>
+            <Navbar />
+            <RouterProvider router={router} />
         </Fragment>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,17 @@ export function Navbar() {
     return (
         <nav className='navbar navbar-dark bg-dark'>
             <div className='container'>
-                <a className='navbar-brand'>Account Manager</a>
+                <a href='/' className='navbar-brand'>Create Account</a>
+                <a href='/DeleteAccount' className='navbar-brand'>Delete Account</a>
+                <div className="nav-item dropdown">
+                    <a class="navbar-brand dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Balances</a>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="/GetBalances">Get Balances</a></li>
+                        <li><a className="dropdown-item" href="/CreditBalance">Credit Balance</a></li>
+                        <li><a className="dropdown-item" href="/DebitBalance">Debit Balance</a></li>
+                        <li><a className="dropdown-item" href="/TransferBalances">Transfer Balance</a></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
