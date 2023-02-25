0c7c78322b760bd8de9616f2da23f62a01d42229
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2023-02-25 18:33:15 -0300

    Added copy icon

--- src/components/BalanceCards.js
+++ src/components/BalanceCards.js
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, {useRef, useState} from "react";
 import {copyTextToClipboard} from "../hooks/copyToClipBoard.js"
+import { faSquareCheck, faCopy } from '@fortawesome/free-solid-svg-icons'
 
 export default function BalanceCards({account}) {
     const [isCopied, setIsCopied] = useState(false);
@@ -25,10 +27,10 @@ export default function BalanceCards({account}) {
             <div className="card-top-part">
             <div className={"status-marker " + ((account.active) ? "enabled" : "disabled")}></div>
             <p className="name">Account ID: </p>
-            <div>
-              <input ref={idInput} readOnly type="text" value={account._id} />
+            <div className="id-input-container">
+              <input className="id-input" ref={idInput} readOnly type="text" value={account._id} />
               <button onClick={handleCopyClick}>
-                <span>{isCopied ? 'Copied!' : 'Copy'}</span>
+                <span>{isCopied ? <FontAwesomeIcon icon={faSquareCheck} /> : <FontAwesomeIcon icon={faCopy} />}</span>
               </button>
             </div>
             </div>
